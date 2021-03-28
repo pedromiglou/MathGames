7b167c3815787949e34dca0607ba12d2cfe0aa60
ricardocruz29 <ricardo.cruz2000@outlook.pt>
2021-03-28 16:07:16 +0100

    Static vertical menu

--- WebApp/mathgames_app/src/Components/Menu/Menu.js
+++ WebApp/mathgames_app/src/Components/Menu/Menu.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Menu.css';
 import {Link} from 'react-router-dom';
-import {sidebarData} from './SidebarData.js';
+import {sidebarData_group1, sidebarData_group2} from './SidebarData.js';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import {IconContext} from 'react-icons';
@@ -25,25 +25,43 @@ function Menu(){
                     </div>
                 </div>
             </div>
-            <nav className={sidebaraa ? "nav-menu active" : "nav-menu"}>
+            {
+            //<hr className="menu-divider"></hr>
+            }
+            <nav className={sidebar ? "nav-menu active" : "nav-menu collapsed"}>
                 <ul className="nav-menu-items">
-                    {/* 
-                    <li className="navbar-toggle">
-                        <Link to="#" className="menu-bars">
-                            <AiIcons.AiOutlineClose onClick={showSidebar}/>
-                        </Link>
-                    </li>
-                    
-                    */}
-                    
-                    {sidebarData.map((item, index) =>{
+                    <div class="nav-item">
+                        <li className="nav-text">
+                            <Link to="/"> 
+                                <i className="subicon"><AiIcons.AiFillHome/></i>
+                                <span className={sidebar ? "icons-name" : "icons-noname"}>Inicio</span>
+                            </Link>
+                        </li>
+                    </div>
+                    <hr></hr>
+                    {sidebarData_group1.map((item, index) =>{
                         return(
-                            <li key={index} className={item.cName}>
-                                <Link to={item.path}>
-                                    {item.icon}
-                                    <span>{item.title}</span>
-                                </Link>
-                            </li>
+                            <div class="nav-item">
+                                <li key={index} className={item.cName}>
+                                    <Link to={item.path}> 
+                                        <i className="subicon">{item.icon}</i>
+                                        <span className={sidebar ? "icons-name" : "icons-noname"}>{item.title}</span>
+                                    </Link>
+                                </li>
+                            </div>
+                        );
+                    })}
+                    <hr></hr>
+                    {sidebarData_group2.map((item, index) =>{
+                        return(
+                            <div class="nav-item">
+                                <li key={index} className={item.cName}>
+                                    <Link to={item.path}> 
+                                        <i className="subicon">{item.icon}</i>
+                                        <span className={sidebar ? "icons-name" : "icons-noname"}>{item.title}</span>
+                                    </Link>
+                                </li>
+                            </div>
                         );
                     })}
                 </ul>
