e9ecb55fdc2b26989ca81fb97478d9e9788903c0
Diogo Cunha <dgcunha@ua.pt>
2021-03-28 16:07:58 +0100

    home_page estatica terminada

--- WebApp/mathgames_app/src/Pages/Welcome/Welcome.js
+++ WebApp/mathgames_app/src/Pages/Welcome/Welcome.js
@@ -5,75 +5,87 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./Welcome.css";
 
 function Welcome() {
-    return (
-        <div className="hero-container">
-            <div className="container">
-                <div className="row">
-                    <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                        <div className="carousel-caption">
-                            <h1>
-                                Bem vindo ao{" "}
-                                <strong className="">MathGames</strong>
-                            </h1>
-                            <p style={{ marginTop: "50px" }}>
-                                Sejam muito bem-vindos à plataforma MathGames.
-                                Aqui podem encontrar vários jogos matemáticos
-                                para jogarem e se divertirem. Podem jogar
-                                competitivamente, criar torneios, jogar com
-                                amigos entre outras coisas. Esperemos que se
-                                divirtam!
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                        <div
-                            className=""
-                            style={{
-                                paddingTop: "85px",
-                                maxWidth: "100%",
-                                height: "auto",
-                            }}
-                        >
-                            <figure>
-                                <img
-                                    src={
-                                        process.env.PUBLIC_URL +
-                                        "/images/mathGames.png"
-                                    }
-                                    alt="logo"
-                                />
-                            </figure>
-                        </div>
-                    </div>
-                    <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                        <div className="row mobile">
-                            <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
-                                <div id="container">
-                                    <div className="hero-btns">
-                                        <Button
-                                            className="btns"
-                                            buttonStyle="btn--primary"
-                                            buttonSize="btn--large"
-                                            address="/choose"
-                                            routing="Link"
-                                        >
-                                            JOGAR AGORA!{" "}
-                                            <i className="far fa-play-circle" />
-                                        </Button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 mobile_buttons">
-                                <div className="row">
-                                    <p> outras fotos aqui</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="hero-container">
+			<div className="container">
+				<div className="row">
+					<div className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+						<div className="hero-text">
+							<h1>
+								Bem vindo ao{" "}
+								<strong className="title">MathGames</strong>
+							</h1>
+							<p>
+								Sejam muito bem-vindos à plataforma MathGames.
+								Aqui podem encontrar vários jogos matemáticos
+								para jogarem e se divertirem. Podem jogar
+								competitivamente, criar torneios, jogar com
+								amigos entre outras coisas. Esperemos que se
+								divirtam!
+							</p>
+						</div>
+					</div>
+					<div className="col-xl-6 col-lg-6 col-md-0 col-sm-0">
+						<div className="img">
+							<figure>
+								<img
+									src={
+										process.env.PUBLIC_URL +
+										"/images/mathGames.png"
+									}
+									alt="logo"
+								/>
+							</figure>
+						</div>
+					</div>
+					<div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+						<div className="row">
+							<div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 centered">
+								<Button
+									className="btns"
+									buttonStyle="btn--primary"
+									buttonSize="btn--large"
+									address="/choose"
+									routing="Link"
+								>
+									JOGAR AGORA!{" "}
+									<i className="far fa-play-circle" />
+								</Button>
+							</div>
+							<div className="half container col-xl-6 col-lg-6 col-md-8 col-sm-12">
+								<div className="row">
+									<div className="half container align-right">
+										<figure>
+											<img
+												className="mobile-icons"
+												src={
+													process.env.PUBLIC_URL +
+													"/images/android.png"
+												}
+												alt="android"
+											/>
+										</figure>
+									</div>
+									<div className="half container">
+										<figure>
+											<img
+												className="mobile-icons"
+												src={
+													process.env.PUBLIC_URL +
+													"/images/ios.png"
+												}
+												alt="ios"
+											/>
+										</figure>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Welcome;
