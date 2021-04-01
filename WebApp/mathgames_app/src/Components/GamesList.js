c9450e199742bcf71c473d9a9eeaa9aad57b68e7
dgCunhaUA <dgcunha@ua.pt>
2021-04-01 11:31:00 +0100

    list game component improve

--- WebApp/mathgames_app/src/Components/GamesList.js
+++ WebApp/mathgames_app/src/Components/GamesList.js
@@ -1,53 +1,95 @@
-import React from "react";
-import { Row, Container, Card } from "react-bootstrap";
+import { React, useState } from "react";
+import { Container, Card } from "react-bootstrap";
+import ReactDOM from 'react-dom'
 
-import "./GamesList";
+import "./GamesList.css";
 
 const games_list = [
-	{
-		id: 0,
-		title: "Game 1",
-		img: process.env.PUBLIC_URL + "/images/mathGames.png",
-	},
-	{
-		id: 1,
-		title: "Game 2",
-		img: process.env.PUBLIC_URL + "/images/mathGames.png",
-	},
-	{
-		id: 2,
-		title: "Game 2",
-		img: process.env.PUBLIC_URL + "/images/mathGames.png",
-	},
-	{
-		id: 3,
-		title: "Game 2",
-		img: process.env.PUBLIC_URL + "/images/mathGames.png",
-	},
-	{
-		id: 4,
-		title: "Game 2",
-		img: process.env.PUBLIC_URL + "/images/mathGames.png",
-	},
+    {
+        id: 0,
+        title: "Game 1",
+        img: process.env.PUBLIC_URL + "/images/mathGames.png",
+    },
+    {
+        id: 1,
+        title: "Game 2",
+        img: process.env.PUBLIC_URL + "/images/mathGames.png",
+    },
+    {
+        id: 2,
+        title: "Game 2",
+        img: process.env.PUBLIC_URL + "/images/mathGames.png",
+    },
+    {
+        id: 3,
+        title: "Game 2",
+        img: process.env.PUBLIC_URL + "/images/mathGames.png",
+    },
+    {
+        id: 4,
+        title: "Game 2",
+        img: process.env.PUBLIC_URL + "/images/mathGames.png",
+    },
 ];
 
-const Teste = () => {
-	return (
-		<Container className="display-games">
-            <Row>
-                {games_list.map((game) => (
-					<Card>
-						<Card.Img
-							variant="top"
-							src={game.img}
-							style={{ margin: "0" }}
-							fluid
-						/>
-					</Card>
-				))}
-            </Row>
-		</Container>
-	);
-};
+function GamesList() {
 
-export default Teste;
+	var element = "";
+
+    function changeCard(e) {
+        e.target.style.width = "350px";
+        e.target.style.height = "350px";
+        e.target.style.opacity = "0.5";
+        e.target.style.backgroundColor = "rgba(0,0,0,.5)";
+
+		const element = (
+			<h1 className='testex'>
+			  Hello !
+			</h1>
+		  );
+		  
+		  ReactDOM.render(
+			element,
+			document.getElementById('root')
+		  );
+    }
+
+    function replaceCard(e) {
+        e.target.style.margin = "0px";
+        e.target.style.width = "300px";
+        e.target.style.height = "300px";
+        e.target.style.opacity = "1";
+        e.target.style.backgroundColor = "white";
+		var wtf = "";
+    }
+
+
+    return (
+        <Container className="display-games container">
+            {games_list.map((game) => (
+                <Card key={game.id}>
+                    {/* <Card.Img
+                        variant="top"
+                        src={game.img}
+                        className="card-img"
+						onMouseOver={changeCard}
+						onMouseLeave={replaceCard}
+                    /> */}
+                    <div>
+                        <img
+                            src={game.img}
+                            alt="Info"
+                            className="card-img"
+							onMouseOver={changeCard}
+							onMouseLeave={replaceCard}
+                        />
+						{element}
+						
+                    </div>
+                </Card>
+            ))}
+        </Container>
+    );
+}
+
+export default GamesList;
--- WebApp/mathgames_app/src/Pages/ChooseGameMode/ChooseGameMode.js
+++ WebApp/mathgames_app/src/Pages/ChooseGameMode/ChooseGameMode.js
@@ -2,14 +2,14 @@ import React from "react";
 // import { Link } from "react-router-dom";
 import GamesList from "../../Components/GamesList.js"
 
+import "./ChooseGameMode.css"
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function ChooseGameMode() {
     return (
-        <>
-            <h1>Game mode</h1>
+        <div>
             <GamesList />
-        </>
+        </div>
     );
 }
 
