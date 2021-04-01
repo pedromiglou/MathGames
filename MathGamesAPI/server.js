d9e3034c2b7e80b99496ceb6b3a3f0711e53d905
Rafael Baptista <rafaelbaptista@ua.pt>
2021-04-01 18:31:27 +0100

    Ban basic operations done

--- MathGamesAPI/server.js
+++ MathGamesAPI/server.js
@@ -15,6 +15,7 @@ require("./app/routes/user.routes.js")(app);
 require("./app/routes/game.routes.js")(app);
 require("./app/routes/gamematch.routes.js")(app);
 require("./app/routes/userranks.routes.js")(app);
+require("./app/routes/ban.routes.js")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
