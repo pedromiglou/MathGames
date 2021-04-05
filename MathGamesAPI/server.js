6241741f2e020667fe48fe3c07e15ff144af3b7d
Rafael Baptista <rafaelbaptista@ua.pt>
2021-04-05 18:12:53 +0100

    Fix Some Express Security Issues

--- MathGamesAPI/server.js
+++ MathGamesAPI/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const helmet = require("helmet");
 
 const app = express();
+app.use(helmet());
 
 app.use(bodyParser.json());
 
