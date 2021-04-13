7199dc73bdac48138883d8f6f6a0d4ea69316719
Pedro <pedro.miguel50@ua.pt>
2021-04-13 22:47:29 +0100

    Refactored rastros code. Added game option to Game.js. Bug fixes.

--- WebApp/mathgames_app/src/Components/Engines/RastrosEngine.js
+++ WebApp/mathgames_app/src/Components/Engines/RastrosEngine.js
@@ -1,25 +1,24 @@
 
 import  React from "react";
-//import { Container, Card } from "react-bootstrap";
 import Phaser from "phaser";
 
-var tipo = null
+var game_type = null
 
 export default class RastrosEngine extends React.Component {
-
-
     constructor(props)  {
         super(props)
-        tipo = props.tipo
+        game_type = props.game_type
     }
 
     componentDidMount() {
 
-        let canvasobj = document.getElementById("rastrosCanvas");
+        let canvasobj = document.getElementById("game_canvas");
+        canvasobj.style.border = "20px solid black";
 
         const config = {
             canvas: canvasobj,
             type: Phaser.WEBGL,
+            autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
             width: 1100,
             height: 750,
             backgroundColor: '#4488aa',
@@ -30,10 +29,9 @@ export default class RastrosEngine extends React.Component {
             }
         }
 
-        var game = new Phaser.Game(config);
+        new Phaser.Game(config);
 
     }
-
 
     preload() {
         this.load.image('square', process.env.PUBLIC_URL + '/game_assets/rastros/square.png');
@@ -45,14 +43,16 @@ export default class RastrosEngine extends React.Component {
     }
     
     create() {
-        var INITIAL_BOARD_POS = 60
-        var DISTANCE_BETWEEN_SQUARES = 105
+        this.INITIAL_BOARD_POS = 60
+        this.DISTANCE_BETWEEN_SQUARES = 105
         // Sound effect played after every move
         this.move_sound = this.sound.add('click', {volume: 0.2});
         // Array that stores the board's clickable positions
-        var positions = []
+        this.positions = []
         // Player which is currently playing (1 or 2)
-        this.current_player = 1;				
+        this.current_player = 1;
+        // True if it's a player's turn, False if it's the AI's turn
+        this.player_turn = true;		
         // True if the player's last click was the moving piece, false otherwise
         var clicked_piece_flag = false
         // Squares which have been blocked
@@ -67,74 +67,74 @@ export default class RastrosEngine extends React.Component {
             for (var pos_x = 0; pos_x < 7; pos_x++) {
                 var pos = pos_y*7+pos_x;
                 if (pos === 6)
-                    positions.push(this.add.image(INITIAL_BOARD_POS + DISTANCE_BETWEEN_SQUARES*pos_x, INITIAL_BOARD_POS+DISTANCE_BETWEEN_SQUARES*pos_y, 'p2').setName(String(pos)).setInteractive());
+                    this.positions.push(this.add.image(this.INITIAL_BOARD_POS + this.DISTANCE_BETWEEN_SQUARES*pos_x, this.INITIAL_BOARD_POS+this.DISTANCE_BETWEEN_SQUARES*pos_y, 'p2').setName(String(pos)).setInteractive());
                 else if (pos === 42)
-                    positions.push(this.add.image(INITIAL_BOARD_POS + DISTANCE_BETWEEN_SQUARES*pos_x, INITIAL_BOARD_POS+DISTANCE_BETWEEN_SQUARES*pos_y, 'p1').setName(String(pos)).setInteractive());
+                    this.positions.push(this.add.image(this.INITIAL_BOARD_POS + this.DISTANCE_BETWEEN_SQUARES*pos_x, this.INITIAL_BOARD_POS+this.DISTANCE_BETWEEN_SQUARES*pos_y, 'p1').setName(String(pos)).setInteractive());
                 else
-                    positions.push(this.add.image(INITIAL_BOARD_POS + DISTANCE_BETWEEN_SQUARES*pos_x, INITIAL_BOARD_POS+DISTANCE_BETWEEN_SQUARES*pos_y, 'square').setName(String(pos)).setInteractive());
+                    this.positions.push(this.add.image(this.INITIAL_BOARD_POS + this.DISTANCE_BETWEEN_SQUARES*pos_x, this.INITIAL_BOARD_POS+this.DISTANCE_BETWEEN_SQUARES*pos_y, 'square').setName(String(pos)).setInteractive());
             }
         }
         
-        console.log(this.INITIAL_BOARD_POS)
         // Fill in the moving piece
-        var player_piece = this.add.image(INITIAL_BOARD_POS + DISTANCE_BETWEEN_SQUARES*4, INITIAL_BOARD_POS+DISTANCE_BETWEEN_SQUARES*2, 'piece').setName('player_piece').setInteractive();
+        var player_piece = this.add.image(this.INITIAL_BOARD_POS + this.DISTANCE_BETWEEN_SQUARES*4, this.INITIAL_BOARD_POS+this.DISTANCE_BETWEEN_SQUARES*2, 'piece').setName('player_piece').setInteractive();
     
         this.add.text(750+20, 60, "É a vez do jogador:", {font: "40px Impact", color: "Orange"});
         var current_player_text = this.add.text(750+95, 120, "Jogador " + this.current_player, {font: "40px Impact", color: "Orange"});
-        
+
         // Triggered when the player clicks
         this.input.on('pointerdown', function(pointer, currentlyOver) {
             var clicked_piece = currentlyOver[0];
-            if (clicked_piece !== undefined) {
+            if ( clicked_piece !== undefined  && this.player_turn)
                 if (clicked_piece.name === "player_piece") {
                     clicked_piece_flag = true;
-                    valid_squares.forEach(square => positions[square].setTint(0x00FF00));
-    
-                } else if (clicked_piece_flag) {
+                    valid_squares.forEach(square => this.positions[square].setTint(0x00FF00));
+                } else if ( clicked_piece_flag ) {
                     clicked_piece_flag = false;
-                    valid_squares.forEach(square => positions[square].clearTint());
-                    move(this, blocked_squares, positions, clicked_piece, current_player_text, last_played, valid_squares, player_piece);
+                    valid_squares.forEach(square => this.positions[square].clearTint());
+                    var is_finished = move(this, blocked_squares, clicked_piece, current_player_text, last_played, valid_squares, player_piece);
+
+                    if ( game_type === "AI" && !is_finished ) {
+                        this.player_turn = false;
+                        
+                        // Process AI move
+                        var ai_move = randomPlay(valid_squares);
+                        clicked_piece = this.positions[ai_move];
+                        var wait_time =  Math.floor(Math.random() * (2000 - 500) ) + 500;
+                        setTimeout(() => {move(this, blocked_squares, clicked_piece, current_player_text, last_played, valid_squares, player_piece); this.player_turn = true;}, wait_time);
+                    }
                 }
-            }
         }, this);
     }
     
     update() {}
-    
-    
 
     render() {
         return (
-            <canvas id="rastrosCanvas" />
+            <canvas id="game_canvas" className="game"/>
         );
-        
     }
 }
-
 
 function move_image(scene, image, new_x, new_y) {
     scene.add.image(image.x, image.y, 'blocked').setName('blocked');
     image.setPosition(new_x, new_y);
 }
 
-function move(scene, blocked_squares, positions, clicked_piece, current_player_text, last_played, valid_squares, player_piece) {
-    console.log(tipo)
-    var INITIAL_BOARD_POS = 60
-    var DISTANCE_BETWEEN_SQUARES = 105
+function move(scene, blocked_squares, clicked_piece, current_player_text, last_played, valid_squares, player_piece) {
     if ( valid_squares.has(clicked_piece.name) ) {
         scene.move_sound.play();
 
         // Update blocked squares
-        var old_x = (player_piece.x-INITIAL_BOARD_POS)/ DISTANCE_BETWEEN_SQUARES;
-        var old_y = (player_piece.y-INITIAL_BOARD_POS)/ DISTANCE_BETWEEN_SQUARES;
+        var old_x = ( player_piece.x - scene.INITIAL_BOARD_POS )/ scene.DISTANCE_BETWEEN_SQUARES;
+        var old_y = ( player_piece.y - scene.INITIAL_BOARD_POS )/ scene.DISTANCE_BETWEEN_SQUARES;
         blocked_squares.add(String(old_y*7+old_x));
 
         // Remove last movement highlight and add new one
-        last_played.forEach(pos =>  positions[pos].clearTint());
+        last_played.forEach( pos => scene.positions[pos].clearTint() );
         last_played.clear();
-        last_played.add(old_y*7+old_x);
-        last_played.add(parseInt(clicked_piece.name));
-        last_played.forEach(pos =>  positions[pos].setTint(0xFFFF00));
+        last_played.add( old_y*7 + old_x );
+        last_played.add( parseInt(clicked_piece.name) );
+        last_played.forEach( pos =>  scene.positions[pos].setTint(0xFFFF00) );
 
         // Move player piece to new square
         move_image(scene, player_piece, clicked_piece.x, clicked_piece.y);
@@ -187,6 +187,7 @@ function move(scene, blocked_squares, positions, clicked_piece, current_player_t
         if (current_pos === 6 || current_pos === 42 || set_diff(valid_squares, blocked_squares).size === 0) {
             finish_game(scene, current_pos);
             valid_squares.clear();
+            return true;
         }   else {
             if (scene.current_player===1)
                 scene.current_player=2;
@@ -194,23 +195,14 @@ function move(scene, blocked_squares, positions, clicked_piece, current_player_t
                 scene.current_player=1;
             
             current_player_text.setText("Jogador " + scene.current_player);
-
-            if (tipo==="AI") {
-                setTimeout(function(){ AI_move(scene, valid_squares, player_piece, blocked_squares, clicked_piece, last_played, positions, current_player_text) }, 1000);
-                //AI_move(scene, valid_squares, player_piece, blocked_squares, clicked_piece, last_played, positions, current_player_text)
-            }
         }
-        
     }
-    
-
+    return false;
 }
 
 
 function set_diff(a, b) {
-    var c = new Set( [...a].filter(x => !b.has(x)) )
-    //console.log("Set Diff: ", c)
-    return c
+    return new Set( [...a].filter(x => !b.has(x)) )
 }
 
 function finish_game(scene, current_pos) {
@@ -231,92 +223,7 @@ function finish_game(scene, current_pos) {
         easeParams: [1.5, 0.5],
         delay: 0
     }, scene);
-}
-
-
-
-
-
-function AI_move(scene, valid_squares, player_piece, blocked_squares, clicked_piece, last_played, positions, current_player_text) {
-    var INITIAL_BOARD_POS = 60
-	var DISTANCE_BETWEEN_SQUARES = 105
-    var AI_square = randomPlay(valid_squares)
-    var poslist = [(AI_square-(AI_square%7))/7, AI_square%7]
-    var pos_x = INITIAL_BOARD_POS + DISTANCE_BETWEEN_SQUARES*poslist[1]
-    var pos_y = INITIAL_BOARD_POS + DISTANCE_BETWEEN_SQUARES*poslist[0]
-
-    // Update blocked squares
-    var old_x = (player_piece.x-INITIAL_BOARD_POS)/ DISTANCE_BETWEEN_SQUARES;
-    var old_y = (player_piece.y-INITIAL_BOARD_POS)/ DISTANCE_BETWEEN_SQUARES;
-    blocked_squares.add(String(old_y*7+old_x));
-
-    // Remove last movement highlight and add new one
-    last_played.forEach(pos =>  positions[pos].clearTint());
-    last_played.clear();
-    last_played.add(old_y*7+old_x);
-    last_played.add(parseInt(clicked_piece.name));
-    last_played.forEach(pos =>  positions[pos].setTint(0xFFFF00));
-
-    // Move player piece to new square
-    move_image(scene, player_piece, pos_x, pos_y);
-
-    
-    // Get new square's position [0..49]
-    var current_pos = parseInt(AI_square)
-    valid_squares.clear()
-
-    // Add all possible positions
-    valid_squares.add(String(current_pos-6));
-    valid_squares.add(String(current_pos-7));
-    valid_squares.add(String(current_pos-8));
-
-    valid_squares.add(String(current_pos+6));
-    valid_squares.add(String(current_pos+7));
-    valid_squares.add(String(current_pos+8));
-
-    valid_squares.add(String(current_pos-1));
-    valid_squares.add(String(current_pos+1));
-    
-    // Remove invalid squares (edge cases)
-    if ( [0,1,2,3,4,5,6].includes(current_pos) ) {
-        valid_squares.delete(String(current_pos-6));
-        valid_squares.delete(String(current_pos-7));
-        valid_squares.delete(String(current_pos-8));
-    }
-
-    if ( [42,43,44,45,46,47,48].includes(current_pos) ) {
-        valid_squares.delete(String(current_pos+6));
-        valid_squares.delete(String(current_pos+7));
-        valid_squares.delete(String(current_pos+8));
-    }
-
-    if ( [0,7,14,21,28,35,42].includes(current_pos) ) {
-        valid_squares.delete(String(current_pos-8));
-        valid_squares.delete(String(current_pos-1));
-        valid_squares.delete(String(current_pos+6));
-    }
-
-    if ( [6,13,20,27,34,41,48].includes(current_pos) ) {
-        valid_squares.delete(String(current_pos-6));
-        valid_squares.delete(String(current_pos+1));
-        valid_squares.delete(String(current_pos+8));
-    }
-
-    // Remove blocked squares
-    blocked_squares.forEach(square => valid_squares.delete(square));
-
-    // Check for win conditions
-    if (current_pos === 6 || current_pos === 42 || set_diff(valid_squares, blocked_squares).size === 0) {
-        finish_game(scene, current_pos);
-        valid_squares.clear();
-    } else {
-        if (scene.current_player===1)
-            scene.current_player=2;
-        else
-            scene.current_player=1;
-        
-        current_player_text.setText("Jogador " + scene.current_player);
-    }
+    scene.positions.forEach(x => x.disableInteractive());
 }
 
 function randomPlay(validSquares) {
@@ -328,7 +235,6 @@ function randomPlay(validSquares) {
             return current;
         }
     });
-   // this.fieldUpdate(chosen);
     return chosen[0]*7+chosen[1];
 }
 
