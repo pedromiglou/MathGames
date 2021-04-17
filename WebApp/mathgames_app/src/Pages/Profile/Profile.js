3c23e50cbc6a22e625667a748a2607b70591afc6
dgCunhaUA <dgcunha@ua.pt>
2021-04-17 15:02:39 +0100

    profile inicial finalizado
    Former-commit-id: e365076f98164f2a1d573cfeff1cfbec1f12bb43

--- WebApp/mathgames_app/src/Pages/Profile/Profile.js
+++ WebApp/mathgames_app/src/Pages/Profile/Profile.js
@@ -6,16 +6,87 @@ import "./Profile.css";
 const Profile = () => {
     const [menuOption, setMenuOption] = useState("Geral");
 
+    var geral_e;
+    var password_e;
+    var rank_e;
+    var last_games_e;
+    var preferencias_e;
+
+
     function geral() {
         setMenuOption("Geral");
 
-        //e.target.style.backg
+        geral_e = document.getElementById("Geral");
+        password_e = document.getElementById("Password");
+        rank_e = document.getElementById("Rank");
+        last_games_e = document.getElementById("Last_Games");
+        preferencias_e = document.getElementById("Preferencias");
+
+        geral_e.style.backgroundColor = "black";
+        password_e.style.backgroundColor = "rgb(63, 63, 63)";
+        rank_e.style.backgroundColor = "rgb(63, 63, 63)";
+        last_games_e.style.backgroundColor = "rgb(63, 63, 63)";
+        preferencias_e.style.backgroundColor = "rgb(63, 63, 63)";
     }
     function password() {
         setMenuOption("Password");
+
+        geral_e = document.getElementById("Geral");
+        password_e = document.getElementById("Password");
+        rank_e = document.getElementById("Rank");
+        last_games_e = document.getElementById("Last_Games");
+        preferencias_e = document.getElementById("Preferencias");
+
+        geral_e.style.backgroundColor = "rgb(63, 63, 63)";
+        password_e.style.backgroundColor = "black";
+        rank_e.style.backgroundColor = "rgb(63, 63, 63)";
+        last_games_e.style.backgroundColor = "rgb(63, 63, 63)";
+        preferencias_e.style.backgroundColor = "rgb(63, 63, 63)";
     }
     function rank() {
         setMenuOption("Rank");
+
+        geral_e = document.getElementById("Geral");
+        password_e = document.getElementById("Password");
+        rank_e = document.getElementById("Rank");
+        last_games_e = document.getElementById("Last_Games");
+        preferencias_e = document.getElementById("Preferencias");
+
+        geral_e.style.backgroundColor = "rgb(63, 63, 63)";
+        password_e.style.backgroundColor = "rgb(63, 63, 63)";
+        rank_e.style.backgroundColor = "black";
+        last_games_e.style.backgroundColor = "rgb(63, 63, 63)";
+        preferencias_e.style.backgroundColor = "rgb(63, 63, 63)";
+    }
+    function last_games() {
+        setMenuOption("lastgames");
+
+        geral_e = document.getElementById("Geral");
+        password_e = document.getElementById("Password");
+        rank_e = document.getElementById("Rank");
+        last_games_e = document.getElementById("Last_Games");
+        preferencias_e = document.getElementById("Preferencias");
+
+        geral_e.style.backgroundColor = "rgb(63, 63, 63)";
+        password_e.style.backgroundColor = "rgb(63, 63, 63)";
+        rank_e.style.backgroundColor = "rgb(63, 63, 63)";
+        last_games_e.style.backgroundColor = "black";
+        preferencias_e.style.backgroundColor = "rgb(63, 63, 63)";
+    }
+    function preferencias() {
+        setMenuOption("Preferencias");
+
+        geral_e = document.getElementById("Geral");
+        password_e = document.getElementById("Password");
+        rank_e = document.getElementById("Rank");
+        last_games_e = document.getElementById("Last_Games");
+        preferencias_e = document.getElementById("Preferencias");
+
+        geral_e.style.backgroundColor = "rgb(63, 63, 63)";
+        password_e.style.backgroundColor = "rgb(63, 63, 63)";
+        rank_e.style.backgroundColor = "rgb(63, 63, 63)";
+        last_games_e.style.backgroundColor = "rgb(63, 63, 63)";
+        preferencias_e.style.backgroundColor = "black";
     }
 
     return (
@@ -26,6 +97,8 @@ const Profile = () => {
                         className="side-button"
                         type="button"
                         onClick={geral}
+                        id="Geral"
+                        style={{backgroundColor: "black"}}
                     >
                         Geral
                     </button>
@@ -33,6 +106,7 @@ const Profile = () => {
                         className="side-button"
                         type="button"
                         onClick={password}
+                        id="Password"
                     >
                         Password
                     </button>
@@ -40,13 +114,24 @@ const Profile = () => {
                         className="side-button"
                         type="button"
                         onClick={rank}
+                        id="Rank"
                     >
                         Rank
                     </button>
-                    <button className="side-button" type="button">
+                    <button
+                        className="side-button"
+                        type="button"
+                        onClick={last_games}
+                        id="Last_Games"
+                    >
                         Ultimos Jogos
                     </button>
-                    <button className="side-button push-down" type="button">
+                    <button
+                        className="side-button push-down"
+                        type="button"
+                        onClick={preferencias}
+                        id="Preferencias"
+                    >
                         Preferencias
                     </button>
                 </div>
@@ -58,6 +143,7 @@ const Profile = () => {
                                     process.env.PUBLIC_URL +
                                     "/images/user-profile.png"
                                 }
+                                alt="profile image"
                             />
                             <div className="input-field_profile">
                                 <input type="text" value="Username" readOnly />
@@ -76,6 +162,78 @@ const Profile = () => {
                 )}
 
                 {menuOption === "Password" && (
+                    <div className="col-lg-9 no-margins">
+                        <div className="container profile">
+                            <div className="input-field_profile">
+                                <input type="text" value="Password" readOnly />
+                            </div>
+                            <div className="input-field_profile">
+                                <input
+                                    type="text"
+                                    value="Password Nova"
+                                    readOnly
+                                />
+                            </div>
+                            <div className="input-field_profile">
+                                <input
+                                    type="text"
+                                    value="Confirmar Password Nova"
+                                    readOnly
+                                />
+                            </div>
+                        </div>
+                    </div>
+                )}
+
+                {menuOption === "Rank" && (
+                    <div className="col-lg-9 no-margins">
+                        <div className="container profile">
+                            <div className="input-field_profile">
+                                <input type="text" value="Password" readOnly />
+                            </div>
+                            <div className="input-field_profile">
+                                <input
+                                    type="text"
+                                    value="Password Nova"
+                                    readOnly
+                                />
+                            </div>
+                            <div className="input-field_profile">
+                                <input
+                                    type="text"
+                                    value="Confirmar Password Nova"
+                                    readOnly
+                                />
+                            </div>
+                        </div>
+                    </div>
+                )}
+
+                {menuOption === "lastgames" && (
+                    <div className="col-lg-9 no-margins">
+                        <div className="container profile">
+                            <div className="input-field_profile">
+                                <input type="text" value="Password" readOnly />
+                            </div>
+                            <div className="input-field_profile">
+                                <input
+                                    type="text"
+                                    value="Password Nova"
+                                    readOnly
+                                />
+                            </div>
+                            <div className="input-field_profile">
+                                <input
+                                    type="text"
+                                    value="Confirmar Password Nova"
+                                    readOnly
+                                />
+                            </div>
+                        </div>
+                    </div>
+                )}
+
+                {menuOption === "Preferencias" && (
                     <div className="col-lg-9 no-margins">
                         <div className="container profile">
                             <div className="input-field_profile">
