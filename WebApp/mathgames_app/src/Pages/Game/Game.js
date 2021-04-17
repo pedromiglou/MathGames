9fb7659b48be5e5408fa9b6ee0c4631ae130e4c4
Diogo Carvalho <dfac@ua.pt>
2021-04-17 16:30:46 +0100

    AI Rastros integration
    Former-commit-id: 0883c87f587b8ace67d1cb07973e26e16884fef9

--- WebApp/mathgames_app/src/Pages/Game/Game.js
+++ WebApp/mathgames_app/src/Pages/Game/Game.js
@@ -3,19 +3,10 @@ import "./Game.css"
 import "bootstrap/dist/css/bootstrap.min.css";
 import RastrosEngine from "../../Components/Engines/RastrosEngine";
 import GatosCaesEngine from "../../Components/Engines/GatosCaesEngine";
-import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://127.0.0.1:4000";
 
 
 function Game() {
-    const [response, setResponse] = useState("");
 
-    useEffect(() => {
-        const socket = socketIOClient(ENDPOINT);
-        socket.on("FromAPI", data => {
-            setResponse(data);
-        });
-    }, []);
 
 
     var game = "rastros";
@@ -24,18 +15,12 @@ function Game() {
     if ( game === "rastros" )
         return (
             <div>           
-                <p>
-                It's <time dateTime={response}>{response}</time>
-                </p> 
                 <RastrosEngine game_type={game_mode}></RastrosEngine>
             </div>
         );
     if ( game === "gatoscaes" )
         return (
             <div>
-                <p>
-                It's <time dateTime={response}>{response}</time>
-                </p>
                 <GatosCaesEngine game_type={game_mode}></GatosCaesEngine>
             </div>
         );
