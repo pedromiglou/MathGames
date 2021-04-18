b0194ec688e4655cab627bce26aa970d14cec5af
Diogo Carvalho <dfac@ua.pt>
2021-04-18 22:35:11 +0100

    multiplayer changes. players turn
    Former-commit-id: aaf244f454ed336f609412d0de62da4f86b6510d

--- MathGamesAPI/server.js
+++ MathGamesAPI/server.js
@@ -20,22 +20,35 @@ const io = require("socket.io")(server, {
     methods: ["GET", "POST"]
   }
 });
-const in_game_users = {};
-const match_queue = {};
-const current_games = {};
+
+
+var in_game_users = {};
+var current_games = {};
+
+var match_queue = [];
+var users_info = {}
 
 io.on("connection", (socket) => { 
   console.log("New client connected");
   console.log("id: ", socket.id);
-  socket.emit('connection');
+  
+  //socket.emit('connection');
 
   socket.on("user_id", (user_id) => {
-    match_queue[user_id] = socket.id;
-    console.log(match_queue);
-    console.log(Object.keys(match_queue).length)
-    if ( Object.keys(match_queue).length >= 2 ) {
+    users_info[user_id] = socket.id;
+    match_queue.push(user_id)
+
+    while ( match_queue.length >= 2 ) {
       console.log("Match found.");
-      create_game(Object.keys(match_queue)[0], Object.keys(match_queue)[1]);
+      var player1 = match_queue.shift()
+      var player2 = match_queue.shift()
+      if (player1 !== undefined && player2 !== undefined) {
+        create_game(player1, player2);
+        break;
+      } else {
+        if (player1 !== undefined) match_queue.unshift(player1)
+        if (player2 !== undefined) match_queue.unshift(player2)
+      }
     }
   });
 
@@ -45,6 +58,7 @@ io.on("connection", (socket) => {
         console.log("Everythings checks out.");
         in_game_users[user_id] = socket.id;
         console.log(in_game_users)
+        console.log(match_queue)
       }
     }
   });
@@ -72,45 +86,9 @@ function create_game(user1, user2) {
     current_games[match_id][user2] = user1;
     current_games[match_id]['started'] = false;
     
-    io.to(match_queue[user1]).emit("match_found", match_id);
-    io.to(match_queue[user2]).emit("match_found", match_id);
+    io.to(users_info[user1]).emit("match_found", {"match_id": match_id, "starter": true});
+    io.to(users_info[user2]).emit("match_found", {"match_id": match_id, "starter": false});
 }
-
-/*
-let interval;
-
-var myconnections = new Set()
-
-io.on("connection", (socket) => {
-  console.log("New client connected");
-  myconnections.add(socket)
-
-  socket.on('client-server', (msg) => {
-    console.log("aqui")
-    console.log(msg)
-    console.log(myconnections.size)
-    myconnections.forEach( element => {
-      console.log("vai")
-      if (socket != element) {
-        element.emit('server-client', msg);
-      }
-    } ) 
-  });
-
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    myconnections.delete(socket)
-  });
-});
-
-
-const getApiAndEmit = socket => {
-  const response = new Date();
-  // Emitting a new message. Will be consumed by the client
-
-  socket.emit("FromAPI", response);
-};
-*/
 
 require("./app/routes/user.routes.js")(app);
 require("./app/routes/game.routes.js")(app);
