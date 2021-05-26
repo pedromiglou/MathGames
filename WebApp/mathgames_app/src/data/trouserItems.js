76d57c457e4a59f1b97b0fb75f49056aeaace833
Diogo Cunha <dgcunha@ua.pt>
2021-05-26 22:56:44 +0100

    Avatar finalizado
    Former-commit-id: 3f0f4c5b3d985dc06317e2207256e2061597043a

--- WebApp/mathgames_app/src/data/trouserItems.js
+++ WebApp/mathgames_app/src/data/trouserItems.js
@@ -3,21 +3,45 @@ export const trouserItems =
         {
             id: 0,
             name: "Trouser1",
-            color: "#808080"            
+            color: "#34495E",
+            lvl: 0            
         },
     
         {
             id: 1,
             name: "Trouser2",
-            color: "#000000"
+            color: "#7B7D7D",
+            lvl: 0
         },
 
         {
             id: 2,
             name: "Trouser3",
-            color: "#ff0000",
+            color: "#EAEDED",
+            lvl: 20
         },
 
+        {
+            id: 3,
+            name: "TrouserJeans",
+            color: "TrouserJeans",
+            img: process.env.PUBLIC_URL + "avatar_assets/texture/TrouserJeans.jpg",
+            lvl: 30
+        },
 
-        
+        {
+            id: 4,
+            name: "TrouserBlackJeans",
+            color: "TrouserBlackJeans",
+            img: process.env.PUBLIC_URL + "avatar_assets/texture/TrouserBlackJeans.jpg",
+            lvl: 40
+        },
+
+        {
+            id: 5,
+            name: "TrouserGrey",
+            color: "TrouserGrey",
+            img: process.env.PUBLIC_URL + "avatar_assets/texture/TrouserGrey.jpg",
+            lvl: 50
+        },
     ];
