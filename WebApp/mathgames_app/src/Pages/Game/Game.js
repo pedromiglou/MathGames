e34dc489002102874e0617ab4c6fd6e38e6c22d7
Pedro Santos <pedro.miguel50@ua.pt>
2021-05-29 18:59:23 +0100

    Game page no longer re-rendering after match is over.
    Former-commit-id: d10c5a76a3eabd84e70e1d13e1e67ee3e1cbab9d

--- WebApp/mathgames_app/src/Pages/Game/Game.js
+++ WebApp/mathgames_app/src/Pages/Game/Game.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from "react";
+import { GameOverModal } from '../../Components/GameOverModal';
 import { Modal } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import "./Game.css"
@@ -12,9 +13,10 @@ function Game()  {
     console.log("Passei aqui")
     //const games_list = useSelector(state => state.matchApp);
     let current_match = useRef(null);
+    const childRef = useRef();
 
-    const [finishMatchModalShow, setFinishMatchModalShow] = useState(false);
-    const [endGameMessage, setEndGameMessage] = useState("");
+    // const [finishMatchModalShow, setFinishMatchModalShow] = useState(false);
+    // const [endGameMessage, setEndGameMessage] = useState("");
 
     let history = useHistory()
     var params = history.location.state
@@ -26,47 +28,48 @@ function Game()  {
     current_match.current = params.match;
 
     function processGameOver(msg) {
-        setEndGameMessage(msg);
-        setFinishMatchModalShow(true);
+        childRef.current.processGameOver(msg, game_id)
+        // setEndGameMessage(msg);
+        // setFinishMatchModalShow(true);
     }
 
-    function FinishMatchModal(props) {
-        return (
-            <Modal {...props} size="md" centered>
-                <Modal.Header style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-                    <Modal.Title style={{color: "#0056b3", fontSize: 30}}>
-                        Jogo Terminado!
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <p style={{color: "#0056b3", fontSize: 20}}>
-                        Resultado: 
-                        { endGameMessage["match_result"]==="win" &&
-                            <span>
-                                Vitória! <span className="smiley-happy"></span>
-                                <br/>
-                                { endGameMessage["end_mode"]==="timeout" && EndGameStatements["win"]["timeout"] }
-                                { endGameMessage["end_mode"]==="valid_move" && EndGameStatements["win"][game_id][endGameMessage["extra"]] }
-                                { endGameMessage["end_mode"]==="invalid_move" && EndGameStatements["win"]["invalidMove"] }
-                            </span>
-                        }
-                        { endGameMessage["match_result"]==="loss" &&
-                            <span>
-                                Derrota. <span className="smiley-sad"></span>
-                                <br/>
-                                { endGameMessage["end_mode"]==="timeout" && EndGameStatements["loss"]["timeout"] }
-                                { endGameMessage["end_mode"]==="valid_move" && EndGameStatements["loss"][game_id][endGameMessage["extra"]] }
-                                { endGameMessage["end_mode"]==="invalid_move" && EndGameStatements["loss"]["invalidMove"] }
-                            </span>
-                        }
-                    </p>
-                </Modal.Body>
-                <Modal.Footer style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-                    <button onClick={() => history.push("/gamePage?id=" + game_id)} className="btn btn-warning" style={{color: "#0056b3", fontSize: 20}}>Voltar à página de jogo</button>
-                </Modal.Footer>
-            </Modal>
-        );
-    }
+    // function FinishMatchModal(props) {
+    //     return (
+    //         <Modal {...props} size="md" centered>
+    //             <Modal.Header style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+    //                 <Modal.Title style={{color: "#0056b3", fontSize: 30}}>
+    //                     Jogo Terminado!
+    //                 </Modal.Title>
+    //             </Modal.Header>
+    //             <Modal.Body>
+    //                 <p style={{color: "#0056b3", fontSize: 20}}>
+    //                     Resultado: 
+    //                     { endGameMessage["match_result"]==="win" &&
+    //                         <span>
+    //                             Vitória! <span className="smiley-happy"></span>
+    //                             <br/>
+    //                             { endGameMessage["end_mode"]==="timeout" && EndGameStatements["win"]["timeout"] }
+    //                             { endGameMessage["end_mode"]==="valid_move" && EndGameStatements["win"][game_id][endGameMessage["extra"]] }
+    //                             { endGameMessage["end_mode"]==="invalid_move" && EndGameStatements["win"]["invalidMove"] }
+    //                         </span>
+    //                     }
+    //                     { endGameMessage["match_result"]==="loss" &&
+    //                         <span>
+    //                             Derrota. <span className="smiley-sad"></span>
+    //                             <br/>
+    //                             { endGameMessage["end_mode"]==="timeout" && EndGameStatements["loss"]["timeout"] }
+    //                             { endGameMessage["end_mode"]==="valid_move" && EndGameStatements["loss"][game_id][endGameMessage["extra"]] }
+    //                             { endGameMessage["end_mode"]==="invalid_move" && EndGameStatements["loss"]["invalidMove"] }
+    //                         </span>
+    //                     }
+    //                 </p>
+    //             </Modal.Body>
+    //             <Modal.Footer style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+    //                 <button onClick={() => history.push("/gamePage?id=" + game_id)} className="btn btn-warning" style={{color: "#0056b3", fontSize: 20}}>Voltar à página de jogo</button>
+    //             </Modal.Footer>
+    //         </Modal>
+    //     );
+    // }
 
     return (
         <div className="container-fluid">
@@ -94,19 +97,20 @@ function Game()  {
                     </div>
                 </div>
                 <div className="col-9">
-                    {!finishMatchModalShow && game_id===0 &&
+                    {game_id===0 &&
                         <div id="my_div_game" className="container-canvas" style={{width: '1100px', height: '577px'}}>
                             <RastrosEngine process_game_over={processGameOver} arg_game_mode={game_mode} arg_ai_diff={ai_diff} curr_match={current_match.current}></RastrosEngine>
                         </div>
                     }
-                    {!finishMatchModalShow && game_id===1 &&
+                    {game_id===1 &&
                         <div id="my_div_game" className="container-canvas" style={{width: '1200px', height: '624px'}}>
                             <GatosCaesEngine process_game_over={processGameOver} arg_game_mode={game_mode} arg_ai_diff={ai_diff} curr_match={current_match.current}></GatosCaesEngine>
                         </div>
                     }
                 </div>
             </div>
-            <FinishMatchModal show={finishMatchModalShow} onHide={() => setFinishMatchModalShow(false)}/>
+            {/* <FinishMatchModal show={finishMatchModalShow} onHide={() => setFinishMatchModalShow(false)}/> */}
+            <GameOverModal ref={childRef}></GameOverModal>
         </div>
     );
 }
