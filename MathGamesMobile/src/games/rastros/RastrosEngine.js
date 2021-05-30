f508fc520afe38a45065d39b754c7bcdb84f9d16
Pedro Amaral <pedromiglou@hotmail.com>
2021-05-30 01:55:24 +0100

    Mobile Rastros AI
    Former-commit-id: cc228c62e723a7232226f7a4e39995f7b65a0189

--- MathGamesMobile/src/games/rastros/RastrosEngine.js
+++ MathGamesMobile/src/games/rastros/RastrosEngine.js
@@ -35,28 +35,6 @@ export default class RastrosEngine extends React.Component {
             <Text style={styles.title}>Player2</Text>
             
         </View>);
-        /*
-        <View style={styles.controls}>
-        <View style={styles.controlRow}>
-            <TouchableOpacity onPress={()=>{this.engine.dispatch({type : "move-up"})}}>
-                <View style={styles.control} />
-            </TouchableOpacity>
-        </View>
-        <View style={styles.controlRow}>
-            <TouchableOpacity onPress={()=>{this.engine.dispatch({type : "move-left"})}}>
-                <View style={styles.control} />
-            </TouchableOpacity>
-            <View style={[styles.control, {backgroundColor: null}]} />
-            <TouchableOpacity onPress={()=>{this.engine.dispatch({type : "move-right"})}}>
-                <View style={styles.control} />
-            </TouchableOpacity>
-        </View>
-        <View style={styles.controlRow}>
-            <TouchableOpacity onPress={()=>{this.engine.dispatch({type : "move-down"})}}>
-                <View style={styles.control} />
-            </TouchableOpacity>
-        </View>
-    </View>*/
     }
 }
 
