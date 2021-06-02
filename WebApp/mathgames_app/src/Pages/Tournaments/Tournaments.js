7940a919dd8ae892bf70ab664a2fbaef9b934235
Ricardo <ricardo.cruz29@ua.pt>
2021-06-02 16:11:06 +0100

    Page Tournaments List done. Missing Create Tournament, Enter Tournament Modals, and Tournament Page
    Former-commit-id: 4ec0558fccd88e8a9abd5c9799f567a44371a9ae

--- WebApp/mathgames_app/src/Pages/Tournaments/Tournaments.js
+++ WebApp/mathgames_app/src/Pages/Tournaments/Tournaments.js
@@ -3,7 +3,16 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Tournaments.css";
 
+import * as FaIcons from 'react-icons/fa';
+import * as RiIcons from 'react-icons/ri';
+import * as BsIcons from 'react-icons/bs';
+
 function Tournaments() {
+    function submitFunction(event) {
+		event.preventDefault();
+		document.getElementById("searchButton").click();
+	}
+
     return (
         <>
             {/* id:
@@ -14,6 +23,262 @@ function Tournaments() {
             game_id:
             winner:
             creator */}
+
+            <div className="list-tournaments shadow3D animation-down">
+				
+				<div className="filters-t">
+					
+						<div className="title-ind-t">
+							<i><RiIcons.RiTrophyFill/></i>
+							<h1>Torneios</h1>
+						</div>
+					
+					<div className="row">
+						<div className="col-12 col-md-12 col-lg-12">
+							<form className="shadow-white" onSubmit={submitFunction}>
+								<div className="form-tournaments">
+									<div className="name-section">
+										<h2>Nome Torneio</h2>
+										
+                                        <input className="form-control form-control-lg" id="filter_username" type="search" placeholder="Procurar pelo nome do torneio"/>
+									</div>
+								
+                                    <div className="privacy-section">
+                                        <h2>Privacidade</h2>
+                                        <div className="checkbox-display">
+                                            <div className="inner-checkbox">
+                                                <input className="form-control form-control-lg" id="publico" type="checkbox" name="Público"/>
+                                                <h5>Público</h5>
+                                            </div>
+                                            
+                                            <div className="inner-checkbox">
+                                                <input className="form-control form-control-lg" id="privado" type="checkbox" name="Privado"/>
+                                                <h5>Privado</h5>
+                                            </div>
+                                                
+                                            
+                                        </div>
+									</div>
+
+                                    <div className="users-section">
+										<h2>Número Utilizadores</h2>
+                                        <input className="form-control form-control-lg" id="filter_allusers_min_level" type="number" placeholder="capacidade"/>
+									</div>
+
+                                    <div className="games-section">
+                                        <h2>Jogos</h2>
+                                        <div className="checkbox-display">
+                                            <div className="inner-checkbox">
+                                                <input className="form-control form-control-lg" id="rastros" type="checkbox" name="Público"/>
+                                                <h5>Rastros</h5>
+                                            </div>
+                                            <div className="inner-checkbox">
+                                                <input className="form-control form-control-lg" id="GC" type="checkbox" name="Privado"/>
+                                                <h5>Gatos&Cães</h5>
+                                            </div>
+                                            
+                                            
+                                        </div>
+                                        
+									</div>
+
+								</div>
+								
+								
+								<button id="searchButton" className="btn btn-lg btn-search" type="button">Procurar <FaIcons.FaSearch/></button>
+							</form>
+							
+						</div>
+					</div>
+				</div>
+                
+                <hr></hr>
+
+                <ul className="list-group">
+                    <li className="list-group-item-t d-flex justify-content-between align-items-center row">
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                            Nome
+                        </div>    
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                            Jogo
+                        </div>
+
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                            Capacidade
+                        </div>
+                        <div className="col-lg-2 col-md-2 col-sm-2">
+                            Privacidade
+                        </div>
+                        <div className="col-lg-1 col-md-1 col-sm-1 join">
+                            
+                        </div>                                
+                    </li>
+
+                    <li className="list-group-item-t d-flex justify-content-between align-items-center row">
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                            Nome do torneio
+                        </div>    
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                            Jogo
+                        </div>
+
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                            0/64
+                        </div>
+                        <div title="Privado" className="col-lg-2 col-md-2 col-sm-2">
+                            <BsIcons.BsFillLockFill/>
+                        </div>
+                        <div title="Entrar" className="col-lg-1 col-md-1 col-sm-1 join">
+                            <FaIcons.FaArrowAltCircleRight/>
+                        </div>                                
+                    </li>
+
+                    <li className="list-group-item-t d-flex justify-content-between align-items-center row">
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                            Nome do torneio
+                        </div>    
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                            Jogo
+                        </div>
+
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                            20/32
+                        </div>
+                        <div title="Privado" className="col-lg-2 col-md-2 col-sm-2">
+                            <BsIcons.BsFillLockFill/>
+                        </div>
+                        <div title="Entrar" className="col-lg-1 col-md-1 col-sm-1 join">
+                            <FaIcons.FaArrowAltCircleRight/>
+                        </div>                                  
+                    </li>
+
+                    <li className="list-group-item-t d-flex justify-content-between align-items-center row">
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                            Nome do torneio
+                        </div>    
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                            Jogo
+                        </div>
+
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                            20/32
+                        </div>
+                        <div title="Público" className="col-lg-2 col-md-2 col-sm-2">
+                            <BsIcons.BsFillUnlockFill/>
+                        </div>
+                        <div title="Entrar" className="col-lg-1 col-md-1 col-sm-1 join">
+                            <FaIcons.FaArrowAltCircleRight/>
+                        </div>                                 
+                    </li>
+
+                    <li className="list-group-item-t d-flex justify-content-between align-items-center row">
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                            Nome do torneio
+                        </div>    
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                            Jogo
+                        </div>
+
+                        <div className="col-lg-3 col-md-3 col-sm-3">
+                            20/32
+                        </div>
+                        <div title="Público" className="col-lg-2 col-md-2 col-sm-2">
+                            <BsIcons.BsFillUnlockFill/>
+                        </div>
+                        <div title="Entrar" className="col-lg-1 col-md-1 col-sm-1 join">
+                            <FaIcons.FaArrowAltCircleRight/>
+                        </div>                                  
+                    </li>
+
+
+
+                    {/* {users.map(function(user, index) {
+                        numberClassificationUsers++;
+                        var contador = 1;
+                        while (true) {
+                            var minimo = contador === 1 ? 0 : 400 * Math.pow(contador-1, 1.1);
+                            var maximo = 400 * Math.pow(contador, 1.1);
+                            if ( (minimo <= user.account_level) && (user.account_level < maximo)) {
+                                break;
+                            }
+                            contador++;
+                        }
+                        return (
+                            <li className="list-group-item d-flex justify-content-between align-items-center row">
+                                <div className="col-lg-1 col-md-1 col-sm-1 align-items-center">
+                                    <span className="badge badge-primary badge-pill">{numberClassificationUsers}</span>
+                                </div>
+                                {
+                                    current_user !== null && current_user["account_type"] === "A" 
+                                    ?  <>
+                                        <div className="col-lg-2 col-md-2 col-sm-2">
+                                            {user.username}
+                                        </div>
+                                        <div className="col-lg-2 col-md-2 col-sm-2">
+                                            {user["account_type"]}
+                                        </div>
+                                        </>
+                                    :
+                                    <div className="col-lg-4 col-md-4 col-sm-4">
+                                        {user.username}
+                                    </div>
+                                }
+                                <div className="col-lg-2 col-md-2 col-sm-2">
+                                    {contador}
+                                </div>
+                                <div className="col-lg-3 col-md-3 col-sm-3">
+                                    {user.account_level} pontos
+                                </div>
+                                <div className="col-lg-2 col-md-2 col-sm-2">
+                                    { current_user !== null && current_user["account_type"] !== "A" && 
+                                        <>
+                                        { friends.length !== 0 &&
+                                            <>
+                                            { friends.some(e => e.id === user.id) &&
+                                                <>
+                                                <i className="subicon pointer"   onClick={() => {setModalUserId(user.id); setModalUsername(user.username); setModalOperation("remove_friend"); setConfirmModalShow(true); setFriendRequestSucess(false); setReportSucess(false); setReportAlreadyMade(false); }}><IoIcons.IoPersonRemove/></i>
+                                                <i className="subicon pointer" style={{marginLeft:"10px"}}  onClick={() => {setModalUserId(user.id); setModalUsername(user.username); setModalOperation("report_player"); setConfirmModalShow(true); setFriendRequestSucess(false); setReportSucess(false); setReportAlreadyMade(false); }}><MdIcons.MdReport/></i>
+                                                </>
+                                            } 
+                                            { (!friends.some(e => e.id === user.id) && user.id !== current_user.id ) &&
+                                                <>
+                                                <i className="subicon pointer"  onClick={() => {setModalUserId(user.id); setModalUsername(user.username); setModalOperation("friend_request"); setConfirmModalShow(true); setFriendRequestSucess(false); setReportSucess(false); setReportAlreadyMade(false); }}><IoIcons.IoPersonAdd/></i>
+                                                <i className="subicon pointer" style={{marginLeft:"10px"}}   onClick={() => {setModalUserId(user.id); setModalUsername(user.username); setModalOperation("report_player"); setConfirmModalShow(true); setFriendRequestSucess(false); setReportSucess(false); setReportAlreadyMade(false); }}><MdIcons.MdReport/></i>
+                                                </>
+                                            } 
+                                            </>	
+                                        }
+                                        { friends.length === 0 &&  user.id !== current_user.id &&
+                                            <>
+                                            <i className="subicon pointer"  onClick={() => {setModalUserId(user.id); setModalUsername(user.username); setModalOperation("friend_request"); setConfirmModalShow(true); setFriendRequestSucess(false); setReportSucess(false); setReportAlreadyMade(false); }}><IoIcons.IoPersonAdd/></i>
+                                            <i className="subicon pointer" style={{marginLeft:"10px"}}   onClick={() => {setModalUserId(user.id); setModalUsername(user.username); setModalOperation("report_player"); setConfirmModalShow(true); setFriendRequestSucess(false); setReportSucess(false); setReportAlreadyMade(false); }}><MdIcons.MdReport/></i>
+                                            </>
+                                            
+                                        }
+                                        </>
+                                    }
+
+
+                                    { current_user !== null && current_user["account_type"] === "A" && user.id !== current_user.id  &&
+                                        <>
+                                    
+                                            <i className="subicon pointer" onClick={() => {setModalUserId(user.id); setModalUsername(user.username); setModalOperation("upgrade"); setConfirmModalShow(true) }}><FaIcons.FaRegArrowAltCircleUp/></i>
+                                            <i className="subicon pointer" style={{marginLeft:"10px"}} onClick={() => {setModalUserId(user.id); setModalUsername(user.username); setModalOperation("downgrade"); setConfirmModalShow(true) }}><FaIcons.FaRegArrowAltCircleDown/></i>
+                                            <i className="subicon pointer" style={{marginLeft:"10px"}}  onClick={() => {setModalUserId(user.id); setModalUsername(user.username); setModalOperation("ban"); setConfirmModalShow(true) }}><IoIcons.IoBan/></i>
+                                            
+                                        </>
+                                    }
+                                    
+                                </div>
+                            </li>
+                        )
+                        })
+                    } */}
+
+                </ul>
+            </div>
+
+
             
         </>
     );
