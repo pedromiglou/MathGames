d7cbb48dae2a10e001cb97efa2866b0084664af6
Diogo Carvalho <dfac@ua.pt>
2021-06-16 00:30:14 +0100

    Swagger API Documentation implementation
    Former-commit-id: 0210330f4d33fdbd91d699263b87e79ef323ee90

--- MathGamesAPI/app/routes/notification.routes.js
+++ MathGamesAPI/app/routes/notification.routes.js
@@ -4,19 +4,89 @@ module.exports = app => {
     const notifications = require("../controllers/notification.controller.js");
     var router = require("express").Router();
   
-    // Create a new notification
+    /**
+     * @swagger
+     * /api/notifications/:
+     *  post:
+     *    description: Creates a new notification
+     *    responses: 
+     *      '200':
+     *         description: Created a new notification with success
+     *      '401':
+     *         description: Unauthorized operation
+     *      '403':
+     *         description: No token provided
+     *      '500':
+     *         description: An internal error has occoured
+     */
     router.post("/", authJwt.verifyToken, notifications.create);
   
-    // Retrieve all notifications
+    /**
+     * @swagger
+     * /api/notifications/:
+     *  get:
+     *    description: Retrieves all notifications
+     *    responses: 
+     *      '200':
+     *         description: Retrieved all notifications with success
+     *      '401':
+     *         description: Unauthorized operation
+     *      '403':
+     *         description: No token provided
+     *      '500':
+     *         description: An internal error has occoured
+     */
     router.get("/", [authJwt.verifyToken, authJwt.isAdmin],  notifications.findAll);
   
-    // Retrieve all notifications a userId has received
+    /**
+     * @swagger
+     * /api/notifications/:id:
+     *  get:
+     *    description: Retrieves a notification by notification id
+     *    responses: 
+     *      '200':
+     *         description: Retrieved required notification with success
+     *      '401':
+     *         description: Unauthorized operation
+     *      '403':
+     *         description: No token provided
+     *      '500':
+     *         description: An internal error has occoured
+     */
     router.get("/:id", authJwt.verifyToken, notifications.findByUserId);
   
-    // Delete a notification
+    /**
+     * @swagger
+     * /api/notifications/:id:
+     *  delete:
+     *    description: Delete a notification by notification id
+     *    responses: 
+     *      '200':
+     *         description: Deleted required notification with success
+     *      '401':
+     *         description: Unauthorized operation
+     *      '403':
+     *         description: No token provided
+     *      '500':
+     *         description: An internal error has occoured
+     */
     router.delete("/:id", authJwt.verifyToken, notifications.delete);
   
-    // Delete all notifications
+    /**
+     * @swagger
+     * /api/notifications/:
+     *  delete:
+     *    description: Delete all notifications 
+     *    responses: 
+     *      '200':
+     *         description: Deleted all notifications with success
+     *      '401':
+     *         description: Unauthorized operation
+     *      '403':
+     *         description: No token provided
+     *      '500':
+     *         description: An internal error has occoured
+     */
     router.delete("/", [authJwt.verifyToken, authJwt.isAdmin], notifications.deleteAll);
   
     app.use('/api/notifications', router);
